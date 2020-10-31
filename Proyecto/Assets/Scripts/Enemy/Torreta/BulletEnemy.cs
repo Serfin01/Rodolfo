d6ecc9822d49ab046fc8f8b49d02a8d0471109f7
Serfin01 <sergilozano96@gmail.com>
2020-10-31 17:16:52 +0100

    calibrar direccion del player y poco mas

---
+++ Proyecto/Assets/Scripts/Enemy/Torreta/BulletEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletEnemy : MonoBehaviour
+{
+    [SerializeField] float bulletForce;
+    [SerializeField] int damage;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, bulletForce * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Player>().Damaged(damage);
+            Destroy(this.gameObject);
+        }
+
+        if (other.CompareTag("Limit"))
+        {
+            Destroy(this.gameObject);
+        }
+
+
+    }
+}
