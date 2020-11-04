d37f4c5ae47e09dcdb216f31313719a7603831b7
Serfin01 <sergilozano96@gmail.com>
2020-11-04 21:00:27 +0100

    script padre de cosas

---
+++ Proyecto/Assets/Scripts/abilities/Scrolls.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Scrolls : MonoBehaviour
+{
+    [SerializeField] GameObject[] typeScrolls;
+    //private int random;
+
+    private int currenIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int newIndex = UnityEngine.Random.Range(0, typeScrolls.Length);
+        typeScrolls[currenIndex].SetActive(false);
+        currenIndex = newIndex;
+        typeScrolls[currenIndex].SetActive(true);
+    }
+
+    enum Abilities
+    {
+        A1, A2, A3, None
+    }
+
+    private Abilities currentAbility = Abilities.None;
+
+    //private Dictionary<int>
+
+    /*
+    // Update is called once per frame
+    void Update()
+    {
+        //random = Random.Range(0, typeScrolls.Length);
+        int newIndex = Random.Range(0, typeScrolls.Length);
+        typeScrolls[currenIndex].SetActive(false);
+        currenIndex = newIndex;
+        typeScrolls[currenIndex].SetActive(true);
+        //Instantiate(typeScrolls[random], transform.position, transform.rotation);
+    }
+    */
+}
