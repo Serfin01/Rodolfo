d16242d42dc10cbdccee165bed671df41e7b0d42
Serfin01 <sergilozano96@gmail.com>
2020-11-10 18:35:19 +0100

    matillo mal

---
+++ Proyecto/Assets/Scripts/Enemy/Dash/DashEnemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashEnemy : MonoBehaviour
+{
+    [SerializeField] private float dashForce;
+    [SerializeField] private float dashDuration;
+    Transform trPlayer;
+    private float rotSpeed = 3.0f;
+    private float moveSpeed = 3.0f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        trPlayer = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(trPlayer.position - transform.position), rotSpeed * Time.deltaTime);
+
+        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(Cast());
+            
+        }
+        /*
+        else
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(trPlayer.position - transform.position), rotSpeed * Time.deltaTime);
+
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        }
+        */
+    }
+
+    public IEnumerator Cast()
+    {
+        //rb.AddForce(transform.forward * dashForce, ForceMode.Impulse);
+        transform.position += transform.forward * dashForce * Time.deltaTime;
+        Debug.Log("dash");
+
+        yield return new WaitForSeconds(dashDuration);
+
+        rb.velocity = Vector3.zero;
+    }
+}
