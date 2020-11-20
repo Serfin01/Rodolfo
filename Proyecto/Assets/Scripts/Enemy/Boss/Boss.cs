3912992ead046c86e8e0e965a072f74b84f33b4f
Serfin01 <sergilozano96@gmail.com>
2020-11-20 15:05:32 +0100

    Update Boss.cs

--- Proyecto/Assets/Scripts/Enemy/Boss/Boss.cs
+++ Proyecto/Assets/Scripts/Enemy/Boss/Boss.cs
@@ -10,23 +10,69 @@ public class Boss : MonoBehaviour
     [SerializeField] float gzRange = 10;
 
     private int maxHealth;
-    public int health = 500;
+    [SerializeField] int health = 500;
     //public GameObject deathEffect;
     public bool isInvulnerable = false;
+    public bool canMove = true;
+    float iniMoveSpeed;
+
+    //float distancia;
+    private float waitTime;
+    [SerializeField] float startWaitTime;
+    [SerializeField] private float recoveryTime;
+    [SerializeField] private float dashTime;
+
+    private int fase = 1;
 
     void Start()
     {
 
         trPlayer = GameObject.FindGameObjectWithTag("Player").transform;
         maxHealth = health;
+        waitTime = startWaitTime;
 
     }
 
     void Update()
     {
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(trPlayer.position - transform.position), rotSpeed * Time.deltaTime);
+        if (canMove == true)
+        {
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(trPlayer.position - transform.position), rotSpeed * Time.deltaTime);
+            transform.LookAt(trPlayer);
 
-        transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+            waitTime -= Time.deltaTime;
+
+            if (waitTime <= 0)
+            {
+                if (fase == 1)
+                {
+                    canMove = false;
+                    StartCoroutine(Fase1());
+                }
+                if (fase == 2)
+                {
+                    canMove = false;
+                    StartCoroutine(Fase2());
+                }
+                waitTime = startWaitTime;
+            }
+            /*
+            distancia = Vector3.Distance(this.transform.position, trPlayer.position);
+
+            if (distancia <= 5)
+            {
+                moveSpeed = 50;
+                if (distancia <= 0.2f)
+                {
+                    canMove = false;
+                    StartCoroutine(Cast());
+                }
+            }
+            */
+
+        }
     }
 
     public void TakeDamage(int damage)
@@ -39,12 +85,59 @@ public class Boss : MonoBehaviour
         if (health <= maxHealth/2)
         {
             //GetComponent<Animator>().SetBool("IsEnraged", true);
+            fase = 2;
         }
 
         if (health <= 0)
         {
             Die();
         }
+    }
+
+    public IEnumerator Fase1()
+    {
+        moveSpeed = 50;
+
+        yield return new WaitForSeconds(dashTime);
+
+        moveSpeed = iniMoveSpeed;
+
+        yield return new WaitForSeconds(recoveryTime);
+
+        canMove = true;
+    }
+
+    public IEnumerator Fase2()
+    {
+        moveSpeed = 50;
+
+        yield return new WaitForSeconds(dashTime);
+
+        moveSpeed = iniMoveSpeed;
+
+        yield return new WaitForSeconds(recoveryTime);
+
+        canMove = true;
+    }
+
+    void RangedFase1()
+    {
+
+    }
+
+    void BulletsFase1()
+    {
+
+    }
+
+    void RangedFase2()
+    {
+
+    }
+
+    void BulletsFase2()
+    {
+
     }
 
     void Die()
