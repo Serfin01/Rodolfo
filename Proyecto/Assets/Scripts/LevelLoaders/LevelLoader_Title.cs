d618926abbe2748815fea7f24daa5e8c539f93f3
victor126 <victorsabariego_bcnvj1ma1920@cev.com>
2020-11-24 20:07:31 +0100

    TITLESCREEN 100% DONE

--- Proyecto/Assets/Scripts/LevelLoaders/LevelLoader_Title.cs
+++ Proyecto/Assets/Scripts/LevelLoaders/LevelLoader_Title.cs
@@ -2,21 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using TMPro;
 
 public class LevelLoader_Title : MonoBehaviour
 {
 	public Animator transition;
 
+	public AudioMixer audioMixer;
+
 	public float transitionTime = 1f;
 
+	public TMP_Dropdown resolutionDropdown;
 
-	void Update()
-	{
-		if (Input.GetMouseButtonDown(0))
-		{
-			LoadNextLevel();
-		}
+	Resolution[] resolutions;
+
+	void Start()
+    {
+		resolutions = Screen.resolutions;
+
+		resolutionDropdown.ClearOptions();
+
+		List<string> options = new List<string>();
+
+		int currentResolutionIndex = 0;
+
+		for(int i = 0; i < resolutions.Length; i++)
+        {
+			string option = resolutions[i].width + "x" + resolutions[i].height;
+			options.Add(option);
+
+			if(resolutions[i].width == Screen.currentResolution.width &&
+			resolutions[i].height == Screen.currentResolution.height)
+            {
+				currentResolutionIndex = i;
+			}
+        }
+
+		resolutionDropdown.AddOptions(options);
+		resolutionDropdown.value = currentResolutionIndex;
+		resolutionDropdown.RefreshShownValue();
 	}
+
+	public void SetResolution (int resolutionIndex)
+    {
+		Resolution resolution = resolutions[resolutionIndex];
+		Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
 	public void LoadNextLevel()
 	{
@@ -32,4 +64,32 @@ public class LevelLoader_Title : MonoBehaviour
 		SceneManager.LoadScene(levelIndex);
 	}
 
+	public void PlayGame()
+    {
+		LoadNextLevel();
+
+	}	
+	
+	public void QuitGame()
+    {
+		Debug.Log("quitGame");
+		Application.Quit();
+
+	}
+
+	public void SetVolume(float volume)
+    {
+		audioMixer.SetFloat("volume", volume);
+    }
+
+	public void SetQuality(int qualityIndex)
+    {
+		QualitySettings.SetQualityLevel(qualityIndex); 
+    }
+
+	public void SetFullscreen (bool isFullscreen)
+    {
+		Screen.fullScreen = isFullscreen;
+
+	}
 }
