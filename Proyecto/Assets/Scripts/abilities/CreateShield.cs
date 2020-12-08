a4c0eb503cc9b3cdda6985bbe3ab21da42a62e55
Serfin01 <sergilozano96@gmail.com>
2020-12-08 19:50:13 +0100

    escudo mejor y faltaria mejorar el rayo

---
+++ Proyecto/Assets/Scripts/abilities/CreateShield.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateShield : MonoBehaviour
+{
+    [SerializeField] GameObject shield;
+    //[SerializeField] GameObject bola;
+
+    //private bool canShoot;
+    
+
+    Ray myRay;
+    RaycastHit hit;
+
+    [SerializeField] float cooldown = 3;
+    float iniCooldown;
+
+    void Start()
+    {
+        //shield.SetActive(false);
+        iniCooldown = cooldown;
+    }
+
+    void Update()
+    {
+        cooldown -= Time.deltaTime;
+        myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(myRay, out hit))
+        {
+            if (cooldown <= 0)
+            {
+                if (Input.GetKeyDown("3"))
+                {
+                    //Instantiate(shield, hit.point, Quaternion.identity);
+                    //Instantiate(bola, hit.point, Quaternion.identity);
+                    //Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    //Instantiate(shield, new Vector3(cursorPos.x, 0, cursorPos.z), Quaternion.identity);
+                    //cooldown = iniCooldown;
+                    InstantiateShield();
+                }
+            }
+        }
+
+    }
+    
+    void InstantiateShield()
+    {
+        Instantiate(shield, hit.point, Quaternion.identity);
+        cooldown = iniCooldown;
+    }
+}
