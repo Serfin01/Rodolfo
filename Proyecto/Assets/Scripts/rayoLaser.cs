b7e7430353c897d55e330d3917c716c2abe0c674
victor126 <victorsabariego_bcnvj1ma1920@cev.com>
2021-01-24 21:19:36 +0100

    sonidos y cosas

---
+++ Proyecto/Assets/Scripts/rayoLaser.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rayoLaser : MonoBehaviour
+{
+
+    private LineRenderer lr;
+
+    private bool canShoot;
+    //[SerializeField] float shotDuration;
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lr.SetPosition(0, transform.position);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, hit.point);
+            }
+        }
+        else lr.SetPosition(1, transform.forward * 5000);
+    }
+    
+}
+/*public class rayoLaser : MonoBehaviour
+{
+
+    private LineRenderer lr;
+    public Camera cam;
+    public LineRenderer lineRenderer;
+    public Transform firePoint;
+    // Use this for initialization
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lr.SetPosition(0, firePoint.position);
+        RaycastHit hit;
+        var mousePos = (Vector3)cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, mousePos);
+            }
+        }
+        else lr.SetPosition(1, transform.forward * 5000);
+    }
+}*/
